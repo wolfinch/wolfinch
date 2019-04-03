f85752557fd52aadbc114f5167020f8c1174c5ab
joshith <joe.cet@gmail.com>
2019-04-02 19:19:35 -0700

    sqlite support impl

--- db/sqlite/sqlite.py
+++ db/sqlite/sqlite.py
@@ -1,50 +1,56 @@
 from utils import *
-from pymongo import MongoClient
+from db_base import DB
+import sqlalchemy as db
 
 log = getLogger ('SQLLITE')
-client = None
-db = None
 
-def init ():
-    '''
-        Db Init
-    '''
-    global client, db
-    client = MongoClient()
-#     client = MongoClient('localhost', 27017)
-    if client == None:
-        log.error ("MongoDb init failed")
-        return False
-    else:
-        log.info ("MongoDb Init Done")
-        db = client['OldMonkDb']
-        return True
+
+class SqliteDb (DB):    
+    def __init__ (self):
+        '''
+            Db Init
+        '''
+        self.engine = db.create_engine('sqlite:///data/OldMonk.sqlite.db')       
+        
+        if self.engine == None :
+            log.error ("sqlite or sqlalchemy init failed")
+            return False
+        else:
+            log.info ("Sqlite Init Done")
+            
+            self.connection = self.engine.connect()
+            self.metadata = db.MetaData()             
+            if self.connection == None or self.metadata == None:
+                log.error ("db connection or metadata init failed")
+                return False
+                
+            return True
+        
+    def clear_db ():
+        global client, db    
+        if (not db):
+            return True
+        
+        #clear db entries
+        client.drop_database("OldMonkDb")
+        
+    def clear_table (table):
+        global client, db    
+        if (not db):
+            return True    
+        db.drop_collection (table)
+        
     
-def clear_db ():
-    global client, db    
-    if (not db):
-        return True
+    def insert_one (entry, table):
+        pass
     
-    #clear db entries
-    client.drop_database("OldMonkDb")
+    def delete_one (entry, table):
+        pass
     
-def clear_table (table):
-    global client, db    
-    if (not db):
-        return True    
-    db.drop_collection (table)
+    def insert_many ():
+        pass
     
-
-def insert_one (entry, table):
-    pass
-
-def delete_one (entry, table):
-    pass
-
-def insert_many ():
-    pass
-
-def delete_many ():
-    pass
+    def delete_many ():
+        pass
 
 #EOF
