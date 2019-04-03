11cc61a184026332c8f71fb7fd96d6e41486fdc6
joshith <jork@cisco.com>
2019-04-03 01:05:06 -0700

    db impl

--- db/sqlite/sqlite.py
+++ db/sqlite/sqlite.py
@@ -1,11 +1,28 @@
+#
+# OldMonk Auto trading Bot
+# Desc: Sqlite db impl
+# Copyright 2018, Joshith Rayaroth Koderi. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from utils import *
-from db_base import DB
+from db import DbBase
 import sqlalchemy as db
 
 log = getLogger ('SQLLITE')
 
 
-class SqliteDb (DB):    
+class SqliteDb (DbBase):    
     def __init__ (self):
         '''
             Db Init
@@ -26,7 +43,7 @@ class SqliteDb (DB):
                 
             return True
         
-    def clear_db ():
+    def clear_db (self):
         global client, db    
         if (not db):
             return True
@@ -34,23 +51,23 @@ class SqliteDb (DB):
         #clear db entries
         client.drop_database("OldMonkDb")
         
-    def clear_table (table):
+    def clear_table (self, table):
         global client, db    
         if (not db):
             return True    
         db.drop_collection (table)
         
     
-    def insert_one (entry, table):
+    def insert_one (self, entry, table):
         pass
     
-    def delete_one (entry, table):
+    def delete_one (self, entry, table):
         pass
     
-    def insert_many ():
+    def insert_many (self):
         pass
     
-    def delete_many ():
+    def delete_many (self):
         pass
 
 #EOF
