18a8b623c223eccf058679e5bdb233d27680bb0a
joshith <jork@cisco.com>
2019-04-03 21:47:52 -0700

    db impl

--- market/order.py
+++ market/order.py
@@ -4,10 +4,19 @@
  Desc: Order Class and TradeRequest class - implementation
  (c) Joshith Rayaroth Koderi
 '''
-from decimal import Decimal
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
 
-#simple class to have the trade request (FIXME: add proper shape)
+from decimal import Decimal
+import db
+
+Base = declarative_base()
+Db = None
+
+
+# simple class to have the trade request (FIXME: add proper shape)
 class TradeRequest:
+
 #    ''' 
 #        Desc: Common exchange neutral Trade Request
 #            class {
@@ -25,11 +34,13 @@ class TradeRequest:
         self.type = Type
         self.price = Price
         self.stop = Decimal(Stop or 0)
+
     def __str__(self):
-        return "{'product':%s, 'side':%s, 'size':%g 'type':%s, 'price':%g, 'stop':%g}"%(
+        return "{'product':%s, 'side':%s, 'size':%g 'type':%s, 'price':%g, 'stop':%g}" % (
             self.product, self.side, self.size, self.type, self.price, self.stop)
 
-class Order:
+
+class Order (Base):
     '''
     Desc: Common/exchange neutral OrderStatus structure
     {
@@ -45,6 +56,24 @@ class Order:
     time         : <order status time>
     }
     '''
+         __tablename__ = 'order_table' #TODO: FIXME: extend with market_product_id
+
+        prim_id = Column(Integer, primary_key=True)
+        id = Column(String(64), index=True, nullable=False)
+        product_id = Column(String(64), index=True, nullable=False)
+        order_type = Column(String(64), index=True, nullable=False)
+        status_type = Column(String(64), index=True, nullable=False)
+        status_reason = Column(String(64), index=True, nullable=False)
+        side = Column(String(64), index=True, nullable=False)
+        request_size = Column(Decimal, default=0)
+        filled_size = Column(Decimal, default=0)
+        remaining_size = Column(Decimal, default=0)
+        price = Column(Decimal, default=0)
+        funds = Column(Decimal, default=0)
+        fees = Column(Decimal, default=0)
+        create_time = Column(String(64))
+        update_time = Column(String(64))    
+        
     def __init__(self, order_id, product_id, status_type, order_type=None, status_reason=None,
                  side=None, request_size=0, filled_size=0, remaining_size=0, price=0, funds=0,
                  fees=0, create_time=None, update_time=None
@@ -52,26 +81,45 @@ class Order:
         self.id = order_id
         self.product_id = product_id
         self.order_type = order_type
-        self.status_type =  status_type
-        self.status_reason  = status_reason
+        self.status_type = status_type
+        self.status_reason = status_reason
         self.side = side
         self.request_size = Decimal (request_size)
         self.filled_size = Decimal(filled_size)
         self.remaining_size = Decimal(remaining_size)
         self.price = Decimal(price)
         self.funds = Decimal(funds)
-        self.fees  = Decimal(fees)
-        self.create_time  = create_time
+        self.fees = Decimal(fees)
+        self.create_time = create_time
         self.update_time = update_time
         
     def __str__ (self):
         return ("{'id':%s, 'product_id':%s, 'side':%s, 'order_type':%s, "
             "'status_type':%s, 'status_reason':%s, 'request_size':%s, "
             "'filled_size':%s, 'remaining_size':%s, 'price':%s, 'funds':%s, "
-            "'fees':%s, 'create_time':%s, 'update_time':%s}")%(
+            "'fees':%s, 'create_time':%s, 'update_time':%s}") % (
             self.id, self.product_id, self.side, self.order_type, self.status_type,
              self.status_reason, self.request_size, self.filled_size, self.remaining_size,
              self.price, self.funds, self.fees, self.create_time, self.update_time)
-    
-#EOF
+            
+    def DbSave (self):
+        global Db
+        if not Db:
+            Db = db.getDb()
+        Db.session.add(self)
+        Db.session.commit()
         
+    @classmethod        
+    def DbGet (cls, order_id):
+        global Db
+        if not Db:
+            Db = db.getDb()        
+        try:
+            result = Db.session.query(cls).filter_by(id=order_id)
+            if result:
+                return result.first()
+        except Exception, e:
+            print(e.message)
+        return None 
+        
+# EOF
