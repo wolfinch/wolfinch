60a9ca89469abbc889d9e26609fe9f667076227a
joshith <joe.cet@gmail.com>
2019-04-10 15:29:51 -0700

    initial LSTM model

--- decision/model_engine.py
+++ decision/model_engine.py
@@ -1,4 +1,3 @@
-#! /usr/bin/env python
 #
 # OldMonk Auto trading Bot
 # Desc: Model engine creates trading model crunching data
@@ -17,5 +16,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+regressor = Sequential()
+
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50))
+regressor.add(Dropout(0.2))
+
+regressor.add(Dense(units = 1))
+
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
 
 #EOF
