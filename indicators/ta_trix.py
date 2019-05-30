788d43e05844a5b107a9885433b7dddb5efe9d4f
joshith <joe.cet@gmail.com>
2019-05-30 14:29:41 -0700

    ta_trix impl.

--- indicators/ta_trix.py
+++ indicators/ta_trix.py
@@ -9,29 +9,29 @@ from indicator import Indicator
 import numpy as np
 import talib
 
-from utils import getLogger
-log = getLogger ('TA_TRIX')
-log.setLevel(log.DEBUG)
+# from utils import getLogger
+# log = getLogger ('TA_TRIX')
+# log.setLevel(log.DEBUG)
 
 class TA_TRIX (Indicator):
     '''
     TRIX - 1-day Rate-Of-Change (ROC) of a Triple Smooth EMA market indicator implementation using TA library
     '''
     
-    def __init__(self, name, period=30):
+    def __init__(self, name, period=90):
         self.name = name
         self.period = period
                 
     def calculate(self, candles):        
         candles_len = len(candles)
-        if candles_len < self.period*3:
+        if candles_len < self.period:
             return Decimal(0)
         
-        val_array = np.array(map(lambda x: float(x['ohlc'].close), candles[-self.period*3:]))
+        val_array = np.array(map(lambda x: float(x['ohlc'].close), candles[-self.period:]))
         
         #calculate trix
-        cur_trix = talib.TRIX (val_array, timeperiod=self.period)
+        cur_trix = talib.TRIX (val_array, timeperiod=self.period/3)
         
-        log.info ("TA_TRIX: %s"%str(cur_trix))
+#         log.critical ("TA_TRIX: %s"%str(cur_trix[-1]))
         return Decimal(cur_trix[-1])
         
