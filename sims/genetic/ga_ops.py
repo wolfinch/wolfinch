e08549fa14b8e9b119d77769716edf04905c0002
joshith <joe.cet@gmail.com>
2019-07-02 13:43:04 -0700

    ga trading cfg impl

--- sims/genetic/ga_ops.py
+++ sims/genetic/ga_ops.py
@@ -39,8 +39,8 @@ def selectOneMax(individual, res_dict):
     res_dict["res"] = fitnessVal,
 #     return fitnessVal,
 
-def createOffSpring(indA, indB):
 
+def createOffSpringStrategy(indA, indB):
     ind1, ind2 = indA,indB
     
     size = len(ind1)
@@ -59,27 +59,64 @@ def createOffSpring(indA, indB):
     indA= ind1
     indB= ind2
     return indA, indB
+
+def createOffSpringTradecfg(indA, indB):
+    ind1, ind2 = indA,indB
+    
+    size = len(ind1)
+    cxpoint1 = random.randint(1, size)
+
+    keys = random.sample(list(ind1), cxpoint1)
+    
+    log.debug ("size: %d cxpoint1: %d keys; %s"%(size, cxpoint1, keys))
+    
+    for key in keys:
+        #swap values
+        log.debug ("swapping key: %s"%(key))
+        tmp = ind1[key]
+        ind1[key], ind2[key] = ind2[key], tmp
+            
+    indA= ind1
+    indB= ind2
+    return indA, indB
+
+def createOffSpring(indA, indB):
+
+    indA["strategy_cfg"], indB["strategy_cfg"] = createOffSpringTradecfg(indA["strategy_cfg"], indB["strategy_cfg"])
+    indA["trading_cfg"], indB["trading_cfg"] = createOffSpringTradecfg(indA["trading_cfg"], indB["trading_cfg"])
+
+    return indA, indB
     
 def createMutantStrategy(indS, indpb):
-        
+    
+    conf = eval_strategy.get_strategy_vars()
     log.debug ("original: %s"%(indS))
     for key in indS.iterkeys():
         rand = random.random()
         if rand < indpb:
-            indS [key] = genParamVal(key)
+            indS [key] = genParamVal(conf, key)
 #             raise Exception("rand: %f %s"%(rand, ind))
     individual = indS
     log.debug ("mutant: %s"%(indS))    
     return individual,  
 
 def createMutantTradecfg(indT, indpb):
+    conf = TradingConfig
         
     log.debug ("original: %s"%(indT))
     for key in indT.iterkeys():
         rand = random.random()
         if rand < indpb:
-            indT [key] = genParamVal(key)
+            indT [key] = genParamVal(conf, key)
 #             raise Exception("rand: %f %s"%(rand, ind))
+
+    if (indT["stop_loss_enabled"] == False):
+        indT["stop_loss_smart_rate"] = False
+        indT["stop_loss_rate"] = 0
+        
+    if (indT["take_profit_enabled"] == False):
+        indT["take_profit_rate"] = 0
+
     individual = indT
     log.debug ("mutant: %s"%(indT))    
     return individual,  
@@ -93,9 +130,7 @@ def createMutant (individual, indpb):
 
 def configGenerator ():
     return {"strategy_cfg": strategyGenerator(), "trading_cfg": tradingcfgGenerator()}
-    
-TradingConfig = {"stop_loss_enabled": False, "stop_loss_smart_rate": False, 'stop_loss_rate': 0,
-                 "take_profit_enabled": False, 'take_profit_rate': 0}
+
 TradingConfig = {
         'stop_loss_enabled' : {'default': True, 'var': {'type': bool}},
         'stop_loss_smart_rate' : {'default': True, 'var': {'type': bool}},
@@ -106,20 +141,19 @@ TradingConfig = {
 def tradingcfgGenerator ():
     cfg_gen = {}
     
-    for param_key, param_val in TradingConfig.iterkeys():
-        if param_key == "stop_loss_enabled" or param_key == "stop_loss_smart_rate" or param_key == "take_profit_rate":
-            cfg_gen[param_key] = random.choice([False, True])
-        elif param_key == "stop_loss_smart_rate" or param_key == "take_profit_rate":
-            var = param_val['var']
-            tp = var['type']
-            if tp == int:
-                r_min = var['min']
-                r_max = var['max']
-                r_step = var.get('step')
-                #get val
-                cfg_gen[param_key] = random.randrange (r_min, r_max+1, r_step)     
-            else:
-                raise ("Unknown trading config param type")
+    print "tC %s"%(TradingConfig)
+    for param_key in TradingConfig.iterkeys():
+        cfg_gen [param_key] = genParamVal(TradingConfig, param_key)
+
+    if (cfg_gen["stop_loss_enabled"] == False):
+        cfg_gen["stop_loss_smart_rate"] = False
+        cfg_gen["stop_loss_rate"] = 0
+        
+    if (cfg_gen["take_profit_enabled"] == False):
+        cfg_gen["take_profit_rate"] = 0
+                
+    log.debug ("strat: %s"%(cfg_gen))
+
     return cfg_gen
 
 def strategyGenerator ():
@@ -131,14 +165,13 @@ def strategyGenerator ():
     strat_gen = {}
     
     for param_key in conf.iterkeys():
-        strat_gen [param_key] = genParamVal(param_key)
+        strat_gen [param_key] = genParamVal(conf, param_key)
 #         yield param_key, val    
 #         yield val
     log.debug ("strat: %s"%(strat_gen))
     return strat_gen
 
-def genParamVal (param_key):
-    conf = eval_strategy.get_strategy_vars()
+def genParamVal (conf, param_key):
         
     param_conf = conf[param_key]
     var = param_conf['var']
@@ -156,6 +189,8 @@ def genParamVal (param_key):
         r_max = var['max']
         r_step = var.get('step')
         val = round(random.uniform (r_min, r_max), 2)
+    elif tp == bool:
+        val = random.choice([False, True])
     elif tp == str:
         raise Exception("Unsupported var type str")
     else:
@@ -164,16 +199,20 @@ def genParamVal (param_key):
     return val
 
 if __name__ == "__main__":
+    import strategy.strategies.ema_rsi as ema_rsi
+    
+    eval_strategy.g_strategy_class = ema_rsi.EMA_RSI
     
     print ("conf: %s"%(eval_strategy.get_strategy_vars()))
     
     indA = configGenerator ()
     indB = configGenerator ()
     
-    print ("indA: %s \n indB:%s "%(indA, indB))
+    print ("\n\nindA: %s \n\n indB:%s "%(indA, indB))
     offA, offB = createOffSpring (indA, indB)
     
-    m = selectOneMax (indA)
+    res_dict = dict()
+    m = selectOneMax (indA, res_dict)
     print ("indA: %s \n indB: %s \n offA: %s \n offB: %s val: %s"%(indA, indB, offA, offB, m))
     
 #EOF
