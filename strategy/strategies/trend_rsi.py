0938549f8b3ce72031be6deb86a36be4fba2a1cc
joshith <joe.cet@gmail.com>
2019-12-06 16:56:00 -0800

    223

--- strategy/strategies/trend_rsi.py
+++ strategy/strategies/trend_rsi.py
@@ -18,7 +18,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with Wolfinch.  If not, see <https://www.gnu.org/licenses/>.
 
-from strategy_base import Strategy
+from .strategy_base import Strategy
 import numpy as np
 
 class TREND_RSI(Strategy):
