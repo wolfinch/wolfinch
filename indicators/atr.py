dc0e3e88849f37729766c9dcb1dc3ff836f3d4ac
joshith <joe.cet@gmail.com>
2020-01-02 12:23:42 -0800

    ATR indicator impl

---
+++ indicators/atr.py
@@ -0,0 +1,47 @@
+# '''
+#  Desc: Average True Range (ATR) using tulip
+#  https://tulipindicators.org/atr
+#  Copyright: (c) 2017-2019 Joshith Rayaroth Koderi
+#  This file is part of Wolfinch.
+# 
+#  Wolfinch is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  Wolfinch is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with Wolfinch.  If not, see <https://www.gnu.org/licenses/>.
+# '''
+
+from .indicator import Indicator
+import numpy as np
+import tulipy as ti
+
+class ATR (Indicator):
+    '''
+    Average True Range (ATR) market indicator implementation using Tulip library
+    '''
+    
+    def __init__(self, name, period=14):
+        self.name = name
+        self.period = period
+                
+    def calculate(self, candles):        
+        candles_len = len(candles)
+        if candles_len < self.period+1:
+            return float(0)
+        
+        high_array = np.array([float(x['ohlc'].hiigh) for x in candles[-(self.period+1):]])
+        low_array = np.array([float(x['ohlc'].low) for x in candles[-(self.period+1):]])        
+        close_array = np.array([float(x['ohlc'].close) for x in candles[-(self.period+1):]])
+        
+        #calculate 
+        cur_atr = ti.atr (high_array, low_array, close_array, period=self.period)
+        
+        return float(cur_atr[-1])
+        
