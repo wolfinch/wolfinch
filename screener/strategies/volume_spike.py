83bcc2b884cebaac712a156d50ef580ee39d79cb
joshith <joe.cet@gmail.com>
2021-03-20 18:55:46 -0700

    screener: ui impl incr

--- screener/strategies/volume_spike.py
+++ screener/strategies/volume_spike.py
@@ -31,17 +31,19 @@ class VOL_SPIKE(Screener):
         log.info ("init: name: %s ticker_kind: %s interval: %d"%(name, ticker_kind, interval))
         super().__init__(name, ticker_kind, interval)
         self.YF = yf.Yahoofin ()
+        li = [
+         {"symbol": "aapl", "last_price": 10.2, "change": "10", "pct_change": "2"},
+         {"symbol": "codx", "last_price": "13.2", "change": "20", "pct_change": "20"}            
+             ]        
+        self.filtered_list = li
     def update(self, sym_list, ticker_stats):
         get_all_tickers_info(self.YF, sym_list, ticker_stats)
         return True
     def screen(self, ticker_stats):
         pass
     def get_screened(self):
-        li = [
-         {"symbol": "aapl", "last_price": "10.2", "change": "10", "pct_change": "2"},
-         {"symbol": "codx", "last_price": "13.2", "change": "20", "pct_change": "20"}            
-             ] 
-        return li
+        self.filtered_list[0]["last_price"] += 1
+        return self.filtered_list
 
 def get_all_tickers_info(yf, sym_list, ticker_stats):
     BATCH_SIZE = 400
